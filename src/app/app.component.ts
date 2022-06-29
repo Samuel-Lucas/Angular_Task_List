67d048ce7417ac628c70d65c9c2bb71d0212be43
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-06-28 21:12:11 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: string = 'Minhas tarefas'
+
+
+  constructor() {
+    this.todos.push('passear com o cão')
+    this.todos.push('ir ao mercado')
+    this.todos.push('cortar o cabelo')
+    this.todos.push(new Date()) 
+  }
+
+  alteraTexto() {
+    this.title = 'Teste'
+  }
 }

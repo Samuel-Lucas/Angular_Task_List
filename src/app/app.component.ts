6f54cf26696388d680f072190c30d57b5439f719
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-06-29 21:40:16 -0300

    persistência local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,12 +22,15 @@ export class AppComponent {
         Validators.required
       ])]
     });
+
+    this.load();
   }
 
   add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false))
+    this.save();
     this.clear();
   }
 
@@ -40,13 +43,28 @@ export class AppComponent {
     if(index != -1) {
       this.todos.splice(index, 1);
     }
+    this.save()
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save()
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save()
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if(data) {
+      this.todos = JSON.parse(data);
+    }
   }
 }
